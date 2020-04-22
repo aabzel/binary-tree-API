92ff8130d276f9e7becb6407aa41012e1112802e
Alexander Barunin <aabzel@yandex.ru>
2020-04-22 23:51:12 +0300

    Update avl_tree.h

--- avl_tree.h
+++ avl_tree.h
@@ -1,9 +1,6 @@
 #ifndef AVL_TREE_H
 #define AVL_TREE_H
 
-//https://github.com/ebiggers/avl_tree/blob/master/avl_tree.h
-//https://github.com/penguincoder/avltree/blob/master/Tree.h
-//https://github.com/penguincoder/avltree/blob/master/AVLTree.c
 #include "bin_tree.h"
 
 #include "avl_tree.h"
